6de2fc5709d86b694c1016908dc3b70ffaaa6e30
skycastle359 <159149960+skycastle359@users.noreply.github.com>
2026-05-24 16:12:38 -0400

    Connection to Frontend

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
+app.use(cors())
 app.use(express.json())
+
 
 let movies = [
   { id: 1, title: "Spider-Man", rating: "PG-13", watched: true },
